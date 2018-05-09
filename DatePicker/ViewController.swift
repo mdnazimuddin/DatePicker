02f62a5d9a1156d30c078a9608be9b44d4b6d52c
iMac <imac@iMacs-iMac.local>
2018-05-10 00:42:19 +0600

    DatePicker Project

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -10,15 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateField: UITextField!
+    let picker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        creatDatePicker()
+    }
+    func creatDatePicker(){
+        // toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        // done
+        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePress))
+        toolbar.setItems([done], animated: false)
+        dateField.inputAccessoryView = toolbar
+        dateField.inputView = picker
+        
+        // formate picker
+        picker.datePickerMode = .date
+        
+    }
+    @objc func donePress(){
+        //formate date
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        let dateFormate = formatter.string(from: picker.date)
+        dateField.text = "\(dateFormate)"
+        self.view.endEditing(true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
